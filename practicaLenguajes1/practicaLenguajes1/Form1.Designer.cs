7a1c41b0fd36d29af4db121d6868c8eddbaee667
Bladimir <stivencasimiro10@gmail.com>
2020-09-17 21:56:20 -0600

    tablas y graficos

--- practicaLenguajes1/practicaLenguajes1/Form1.Designer.cs
+++ practicaLenguajes1/practicaLenguajes1/Form1.Designer.cs
@@ -37,15 +37,24 @@
             this.cerrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Area1 = new System.Windows.Forms.RichTextBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -74,39 +83,42 @@
             // comoToolStripMenuItem
             // 
             this.comoToolStripMenuItem.Name = "comoToolStripMenuItem";
-            this.comoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.comoToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.comoToolStripMenuItem.Text = "Crear Archivo";
+            this.comoToolStripMenuItem.Click += new System.EventHandler(this.comoToolStripMenuItem_Click);
             // 
             // estasToolStripMenuItem
             // 
             this.estasToolStripMenuItem.Name = "estasToolStripMenuItem";
-            this.estasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.estasToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.estasToolStripMenuItem.Text = "Abrir";
             this.estasToolStripMenuItem.Click += new System.EventHandler(this.estasToolStripMenuItem_Click);
             // 
             // eLIMINARToolStripMenuItem
             // 
             this.eLIMINARToolStripMenuItem.Name = "eLIMINARToolStripMenuItem";
-            this.eLIMINARToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.eLIMINARToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.eLIMINARToolStripMenuItem.Text = "Editar";
             // 
             // eliminarToolStripMenuItem1
             // 
             this.eliminarToolStripMenuItem1.Name = "eliminarToolStripMenuItem1";
-            this.eliminarToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.eliminarToolStripMenuItem1.Size = new System.Drawing.Size(146, 22);
             this.eliminarToolStripMenuItem1.Text = "Eliminar";
             // 
             // cerrarToolStripMenuItem
             // 
             this.cerrarToolStripMenuItem.Name = "cerrarToolStripMenuItem";
-            this.cerrarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cerrarToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.cerrarToolStripMenuItem.Text = "Cerrar";
+            this.cerrarToolStripMenuItem.Click += new System.EventHandler(this.cerrarToolStripMenuItem_Click);
             // 
             // guardarToolStripMenuItem
             // 
             this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
             this.guardarToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
             this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
             // 
             // splitContainer1
             // 
@@ -118,36 +130,59 @@
             // splitContainer1.Panel1
             // 
             this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.splitContainer1.Panel1.Controls.Add(this.textBox2);
-            this.splitContainer1.Panel1.Controls.Add(this.label2);
-            this.splitContainer1.Panel1.Controls.Add(this.label1);
-            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
-            this.splitContainer1.Panel1.Controls.Add(this.richTextBox1);
-            this.splitContainer1.Size = new System.Drawing.Size(1050, 596);
-            this.splitContainer1.SplitterDistance = 381;
+            this.splitContainer1.Panel1.Controls.Add(this.Area1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1050, 497);
+            this.splitContainer1.SplitterDistance = 295;
             this.splitContainer1.TabIndex = 1;
             // 
-            // richTextBox1
+            // Area1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(4, 4);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(1043, 356);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.Area1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Area1.Location = new System.Drawing.Point(0, 0);
+            this.Area1.Name = "Area1";
+            this.Area1.Size = new System.Drawing.Size(1050, 295);
+            this.Area1.TabIndex = 0;
+            this.Area1.Text = "";
             // 
-            // textBox1
+            // splitContainer2
             // 
-            this.textBox1.Location = new System.Drawing.Point(41, 358);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(17, 20);
-            this.textBox1.TabIndex = 1;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.textBox2);
+            this.splitContainer2.Panel1.Controls.Add(this.label1);
+            this.splitContainer2.Panel1.Controls.Add(this.label2);
+            this.splitContainer2.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer2.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer2_Panel1_Paint);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer2.Size = new System.Drawing.Size(1050, 198);
+            this.splitContainer2.SplitterDistance = 25;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, -2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(17, 20);
+            this.textBox2.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(12, 361);
+            this.label1.Location = new System.Drawing.Point(12, 5);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(23, 13);
             this.label1.TabIndex = 2;
@@ -159,25 +194,38 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label2.Location = new System.Drawing.Point(64, 361);
+            this.label2.Location = new System.Drawing.Point(64, 5);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(48, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Columna";
             // 
-            // textBox2
+            // textBox1
             // 
-            this.textBox2.Location = new System.Drawing.Point(118, 358);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(17, 20);
-            this.textBox2.TabIndex = 4;
+            this.textBox1.Location = new System.Drawing.Point(41, -2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(17, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1050, 176);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(1050, 620);
+            this.ClientSize = new System.Drawing.Size(1050, 521);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -186,9 +234,14 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -205,11 +258,15 @@
         private System.Windows.Forms.ToolStripMenuItem cerrarToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
         private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox Area1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- practicaLenguajes1/practicaLenguajes1/Form1.cs
+++ practicaLenguajes1/practicaLenguajes1/Form1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//agregado por my
+using System.IO;
 
 namespace practicaLenguajes1
 {
     public partial class Form1 : Form
     {
+        string archivo;
         public Form1()
         {
             InitializeComponent();
@@ -19,10 +22,73 @@ namespace practicaLenguajes1
 
         private void estasToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                OpenFileDialog cuadroDialogo = new OpenFileDialog();
+                cuadroDialogo.Filter = "Texto|*.gt";
+                if (cuadroDialogo.ShowDialog() == DialogResult.OK)
+                {
+                    archivo = cuadroDialogo.FileName;
+                    using(StreamReader lectura=new StreamReader(archivo))
+                    {
+                        Area1.Text = lectura.ReadToEnd();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("error de abrir archivo");
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            SaveFileDialog Guardar = new SaveFileDialog();
+            Guardar.Filter = "Texto|*.gt";
+                if (archivo != null)
+                {
+                    using(StreamWriter sobreEscribir=new StreamWriter(archivo))
+                    {
+                        sobreEscribir.Write(Area1.Text);
+                    }
+                }
+                else
+                {
+                    if (Guardar.ShowDialog() == DialogResult.OK)
+                    {
+                        archivo =Guardar.FileName;
+                        using(StreamWriter escribir=new StreamWriter(Guardar.FileName))
+                        {
+                            escribir.Write(Area1.Text);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("error al guardar");
+            }
+        }
+
+        private void comoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Area1.Clear();
+            archivo = null;
+        }
+
+        private void cerrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void splitContainer2_Panel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
